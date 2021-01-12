79b9ef7fe158f5d10203bd466f81335c13219dde
taalojarvi <sreedevan05@gmail.com>
2021-01-12 12:02:05 +0530

    buildscript: Import colors from Firekernel Script

--- newbuild.sh
+++ newbuild.sh
@@ -1,5 +1,11 @@
 #!/bin/bash -e
 
+# Colours
+blue='\033[0;34m'
+cyan='\033[0;36m'
+yellow='\033[0;33m'
+red='\033[0;31m'
+nocol='\033[0m'
 
 # Export Environment Variables
 export PATH="$(pwd)/proton-clang/bin:$PATH"
