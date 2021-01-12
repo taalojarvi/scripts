4311bee4644704a1ed7cecd57f9edc2c4e91067c
taalojarvi <sreedevan05@gmail.com>
2021-01-12 12:11:14 +0530

    buildscript: Functionize all the actions

--- newbuild.sh
+++ newbuild.sh
@@ -19,39 +19,48 @@ export ZipID=$TRAVIS_BUILD_ID
 export KERNEL_IMG=output/arch/arm64/boot/Image.gz-dtb
 
 # Create Release Notes
-touch releasenotes.md
-echo -e "This is an Automated Build of Stratosphere Kernel. Flash at your own risk!" >> releasenotes.md
-echo -e >> releasenotes.md
-echo -e "Build Information" >> releasenotes.md
-echo -e >> releasenotes.md
-echo -e "Build Server Name: "$TRAVIS_APP_HOST >> releasenotes.md
-echo -e "Build ID: "$TRAVIS_BUILD_ID >> releasenotes.md
-echo -e "Build URL: "$TRAVIS_BUILD_WEB_URL >> releasenotes.md
-echo -e >> releasenotes.md
-echo -e "Last 5 Commits before Build:-" >> releasenotes.md
-git log --decorate=auto --pretty=reference --graph -n 10 >> releasenotes.md
-cp releasenotes.md ~/build/Stratosphere-Kernel/android_kernel_nokia_sdm660/Stratosphere-Canaries
+function make_releasenotes()  {
+	touch releasenotes.md
+	echo -e "This is an Automated Build of Stratosphere Kernel. Flash at your own risk!" >> releasenotes.md
+	echo -e >> releasenotes.md
+	echo -e "Build Information" >> releasenotes.md
+	echo -e >> releasenotes.md
+	echo -e "Build Server Name: "$TRAVIS_APP_HOST >> releasenotes.md
+	echo -e "Build ID: "$TRAVIS_BUILD_ID >> releasenotes.md
+	echo -e "Build URL: "$TRAVIS_BUILD_WEB_URL >> releasenotes.md
+	echo -e >> releasenotes.md
+	echo -e "Last 5 Commits before Build:-" >> releasenotes.md
+	git log --decorate=auto --pretty=reference --graph -n 10 >> releasenotes.md
+	cp releasenotes.md ~/build/Stratosphere-Kernel/android_kernel_nokia_sdm660/Stratosphere-Canaries
+}
 
 # Make defconfig
-make stratosphere_defconfig CC=clang O=output/
+function make_defconfig()  {
+	make stratosphere_defconfig CC=clang O=output/
+}
 
 # Make Kernel
-make -j16 CC=clang AR=llvm-ar NM=llvm-nm STRIP=llvm-strip O=output/
-
+function make_kernel  {
+	make -j16 CC=clang AR=llvm-ar NM=llvm-nm STRIP=llvm-strip O=output/
 # Check if Image.gz-dtb exists. If not, stop executing.
-if ! [ -a $KERNEL_IMG ];
-  then
-    echo -e "An error has occured during compilation. Please check your code."
-    exit 1
-  fi 
+	if ! [ -a $KERNEL_IMG ];
+ 		then
+    		echo -e "An error has occured during compilation. Please check your code."
+    		exit 1
+  	fi 
+}
 
 # Make Flashable Zip
-cp output/arch/arm64/boot/Image.gz-dtb AnyKernel3
-cd AnyKernel3
-zip -r9 UPDATE-AnyKernel2.zip * -x README UPDATE-AnyKernel2.zip
-mv UPDATE-AnyKernel2.zip Stratosphere-Kernel-$ZipID.zip
-cp Stratosphere-Kernel-$ZipID.zip ~/build/Stratosphere-Kernel/android_kernel_nokia_sdm660/Stratosphere-Canaries
-cd ~/build/Stratosphere-Kernel/android_kernel_nokia_sdm660/Stratosphere-Canaries
+function make_package()  {
+	cp output/arch/arm64/boot/Image.gz-dtb AnyKernel3
+	cd AnyKernel3
+	zip -r9 UPDATE-AnyKernel2.zip * -x README UPDATE-AnyKernel2.zip
+	mv UPDATE-AnyKernel2.zip Stratosphere-Kernel-$ZipID.zip
+	cp Stratosphere-Kernel-$ZipID.zip ~/build/Stratosphere-Kernel/android_kernel_nokia_sdm660/Stratosphere-Canaries
+	cd ~/build/Stratosphere-Kernel/android_kernel_nokia_sdm660/Stratosphere-Canaries
+}
 
 # Upload Flashable Zip to GitHub Releases <3
+function release()  {
 gh release create ci-$TRAVIS_BUILD_ID Stratosphere-Kernel-$ZipID.zip -F releasenotes.md -p -t "Stratosphere Kernel: Automated Build"
+}
