1c3c3b136706c82cd2e1d456cf84c3732f74e51e
taalojarvi <sreedevan05@gmail.com>
2021-01-18 19:06:29 +0530

    buildscript: Pull updates from kernel repo

--- newbuild.sh
+++ newbuild.sh
@@ -10,15 +10,15 @@ nocol='\033[0m'
 
 # Kernel details
 KERNEL_NAME="Stratosphere"
-VERSION="ME"
+VERSION="Kernel"
 DATE=$(date +"%d-%m-%Y-%I-%M")
 DEVICE="NOKIA_SDM660"
 FINAL_ZIP=$KERNEL_NAME-$VERSION-$DATE.zip
 DEFCONFIG=stratosphere_defconfig
 
 # Dirs and Files
-BASE_DIR=~/
-KERNEL_DIR=$BASE_DIR/android_kernel_nokia_sdm660
+BASE_DIR=$HOME
+KERNEL_DIR=$(pwd)
 ANYKERNEL_DIR=$BASE_DIR/AnyKernel3
 UPLOAD_DIR=$BASE_DIR/Stratosphere-Canaries
 TC_DIR=$BASE_DIR/proton-clang
@@ -27,11 +27,15 @@ OUTPUT=$BASE_DIR/output
 KERNEL_IMG=$OUTPUT/arch/arm64/boot/Image.gz-dtb
 LOG_DIR=$BASE_DIR/logs
 
+
 # Export Environment Variables
-export PATH="$BASE_DIR/proton-clang/bin:$PATH"
+# export PATH="$TC_DIR/bin:$PATH"
+PATH="$TC_DIR/bin:$HOME/linaro-gcc/bin${PATH}"
+export CLANG_TRIPLE="aarch64-linux-gnu-"
 export ARCH=arm64
 export CROSS_COMPILE=aarch64-linux-gnu-
 export CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+export LD_LIBRARY_PATH=$TC_DIR/lib
 export KBUILD_BUILD_USER="Taalojarvi"
 export KBUILD_BUILD_HOST="ASUS-PC"
 export USE_HOST_LEX=yes
@@ -48,6 +52,7 @@ function make_releasenotes()  {
 	echo -e >> releasenotes.md
 	echo -e "Builder: "$KBUILD_BUILD_USER >> releasenotes.md
 	echo -e "Machine: "$KBUILD_BUILD_HOST >> releasenotes.md
+	echo -e "Build Date: "$DATE >> releasenotes.md
 	echo -e >> releasenotes.md
 	echo -e "Last 5 Commits before Build:-" >> releasenotes.md
 	git log --decorate=auto --pretty=reference --graph -n 10 >> releasenotes.md
@@ -61,22 +66,24 @@ function make_defconfig()  {
 
 # Make Kernel
 function make_kernel  {
-	make -j$(nproc --all) CC='ccache clang' AR=llvm-ar NM=llvm-nm STRIP=llvm-strip O=$OUTPUT 2>&1 | tee $LOG_DIR/$DATE.log
+	make -j$(nproc --all) CC='ccache clang  -Qunused-arguments -fcolor-diagnostics' O=$OUTPUT
 # Check if Image.gz-dtb exists. If not, stop executing.
 	if ! [ -a $KERNEL_IMG ];
  		then
     		echo -e "An error has occured during compilation. Please check your code."
     		exit 1
+    	else
+    		echo -e "Kernel built succesfully!"
   	fi 
 }
 
 # Make Flashable Zip
 function make_package()  {
-	cp $OUTPUT/arch/arm64/boot/Image.gz-dtb $ANYKERNEL_DIR
+	cp $KERNEL_IMG $ANYKERNEL_DIR
 	cd $ANYKERNEL_DIR
 	zip -r9 UPDATE-AnyKernel2.zip * -x README UPDATE-AnyKernel2.zip
-	mv UPDATE-AnyKernel2.zip $FINAL_ZIP.zip
-	cp $FINAL_ZIP.zip $UPLOAD_DIR
+	mv UPDATE-AnyKernel2.zip $FINAL_ZIP
+	cp $FINAL_ZIP $UPLOAD_DIR
 	cd $KERNEL_DIR
 	
 }
@@ -84,7 +91,7 @@ function make_package()  {
 # Upload Flashable Zip to GitHub Releases <3
 function release()  {
 cd $UPLOAD_DIR
-gh release create pr-$DATE $FINAL_ZIP.zip -F releasenotes.md -p -t "Stratosphere Kernel: Personal Build"
+gh release create pr-$DATE $FINAL_ZIP -F releasenotes.md -p -t "Stratosphere Kernel: Personal Build"
 cd $KERNEL_DIR
 }
 
@@ -97,12 +104,24 @@ function make_cleanup()  {
 
 # Check for Script Artifacts from previous builds
 function artifact_check()  {
-	if [ -f "$UPLOAD_DIR/releasenotes.md" ]; then
-		rm $UPLOAD_DIR/releasenotes.md
+	if [ -f $ANYKERNEL_DIR/*.zip ]; then
+		 rm $ANYKERNEL_DIR/*.zip
+	else
+		echo -e "Script did not find stale packages. Skipping!"
+	fi
+	
+	if [ -f "$ANYKERNEL_DIR/releasenotes.md" ]; then
+		rm $ANYKERNEL_DIR/releasenotes.md
 	elif [ -f "$KERNEL_DIR/releasenotes.md" ]; then
 		rm $KERNEL_DIR/releasenotes.md	
 	else
-		echo -e "No Script Artifacts found! Skipping."
+		echo -e "No Script Artifacts found. Skipping!"
+	fi
+	
+	if [ -f "$ANYKERNEL_DIR/Image.gz-dtb" ]; then
+		rm $ANYKERNEL_DIR/Image.gz-dtb
+	else
+		echo -e "No stale Kernel Image found. Skipping"
 	fi
 }
 
@@ -136,14 +155,14 @@ function menu()  {
 	echo -e "What do you want to do?"
 	echo -e ""
 	echo -e "1: Build Kernel and Release it to Github"
-	echo -e "2: Build Clean Kernel but Do not release it"
+	echo -e "2: Build Clean Kernel but do not release it"
 	echo -e "3: Build Dirty Kernel "
 	echo -e "4: Make defconfig and open menuconfig"
 	echo -e "5: Make defconfig only"
-	echo -e "6: Cleanup Build Artifacts"
+	echo -e "6: Cleanup script artifacts"
 	echo -e "7: Clear ccache and reset stats"
 	echo -e "8: Regenerate defconfig"
-	echo -e "9: Exit Script"
+	echo -e "9: Exit script"
 	echo -e ""
 	echo -e "Awaiting User Input: "
 	read choice
@@ -158,6 +177,7 @@ function menu()  {
 	 	   make_kernel
 	 	   make_package
 	 	   release
+	 	   artifact_check
 	 	   ;;
 		2) echo -e "Building "$KERNEL_NAME "Kernel" 
 		   make_cleanup
@@ -166,6 +186,7 @@ function menu()  {
 		   make_defconfig
 	 	   make_kernel
 	 	   make_package 
+	 	   artifact_check
 	 	   ;;
 	 	3) echo -e "Building "$KERNEL_NAME "Kernel" 
 	 	   make_defconfig
@@ -193,12 +214,14 @@ function menu()  {
 	 	   regen_defconfig
 	 	   menu
 	 	   ;;
+	 	9) echo -e "Exiting"
+	 	   clear
+	 	   ;; 
 	 	   
 	 esac
 	
 }
 
-echo -e "Checking for artifacts from previous builds and removing them if necessary"
 menu
 BUILD_END=$(date +"%s")
 DIFF=$(($BUILD_END - $BUILD_START))
