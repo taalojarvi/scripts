cdb0bd6277c73d1e775a1514a5caeb1baaf089a5
taalojarvi <sreedevan05@gmail.com>
2021-03-10 13:16:15 +0530

    scripts: Add Circle-CI configuration files
    Signed-off-by: taalojarvi <sreedevan05@gmail.com>

---
+++ circleci/build.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+echo "Cloning dependencies"
+git clone --depth=1 https://github.com/kdrag0n/proton-clang  clang
+git clone --depth=1 https://github.com/taalojarvi/AnyKernel3 AnyKernel
+git clone --depth=1 https://github.com/Stratosphere-Kernel/Stratosphere-Canaries canary
+echo "Done"
+IMAGE=$(pwd)/out/arch/arm64/boot/Image.gz-dtb
+TANGGAL=$(date +"%F-%S")
+START=$(date +"%s")
+KERNEL_DIR=$(pwd)
+PATH="${KERNEL_DIR}/clang/bin:${PATH}"
+export ARCH=arm64
+export KBUILD_BUILD_USER="taalojarvi"
+export KBUILD_BUILD_HOST="app.circleci.com"
+export ARCH=arm64 
+export CROSS_COMPILE=aarch64-linux-gnu-
+export CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+
+# Make Release notes
+function note() {
+touch releasenotes.md
+echo -e "This is an Automated Early Access build of Stratosphere Kernel. Flash at your own risk!" >> releasenotes.md
+echo -e >> releasenotes.md
+echo -e "Build Information" >> releasenotes.md
+echo -e >> releasenotes.md
+echo -e "Build Name: "$CIRCLE_JOB >> releasenotes.mod
+echo -e "Build Number: "$CIRCLE_BUILD_NUM >> releasenotes.md
+echo -e "Build URL: "$CIRCLE_BUILD_URL >> releasenotes.md
+echo -e "Build Date: $(date +%c)" >> releasenotes.md
+echo -e >> releasenotes.md
+echo -e "Last 5 Commits before Build:-" >> releasenotes.md
+echo -e >> releasenotes.md
+git log --decorate=auto --pretty=format:'%Creset %f %C(bold blue)<%an>%Creset %n' --graph -n 10 >> releasenotes.md
+echo -e >> releasenotes.md
+echo -e "Downloads available at https://www.github.com/Stratosphere-Kernel/Stratosphere-Canaries" >> releasenotes.md
+cp releasenotes.md canary/
+}
+
+# Compiling
+function compile() {
+    make CC=clang O=out/ ARCH=arm64 stratosphere_defconfig
+    make -j$(nproc --all) CC=clang AR=llvm-ar NM=llvm-nm STRIP=llvm-strip O=out/
+    if ! [ -a "$IMAGE" ]; then
+        echo -e "Failed! Check your code"
+        exit 1
+    fi
+    cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
+}
+
+# Zipping
+function zipping() {
+    cd AnyKernel || exit 1
+    zip -r9 Stratosphere-${TANGGAL}.zip *
+    cp Stratosphere-${TANGGAL}.zip ../canary
+    cd ../canary
+}
+
+# Releasing
+function release() {
+gh release create earlyaccess-${TANGGAL} Stratosphere-${TANGGAL}.zip -F releasenotes.md -p -t "Stratosphere Kernel: Automated Build"
+}
+note
+compile
+zipping
+release
+END=$(date +"%s")
+DIFF=$(($END - $START))
