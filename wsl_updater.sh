a9694ccede6c91951cf9b6729fbda72bb0147714
taalojarvi <sreedevan05@gmail.com>
2022-06-15 12:31:43 +0530

    wsl_updater.sh: Add a function to handle script failures.
    This has the unintended side effect of "verifying" if the local kernel image is valid and exits if not.
    Might fix in an upcoming commit
    
    Subprocess killer reference:
    https://stackoverflow.com/questions/9893667/how-to-effectively-abort-the-execution-of-a-bash-script-from-a-function
    
    Pipefail reference:
    https://petereisentraut.blogspot.com/2010/11/pipefail.html
    
    Signed-off-by: taalojarvi <sreedevan05@gmail.com>

--- wsl_updater.sh
+++ wsl_updater.sh
@@ -1,7 +1,12 @@
-#!/bin/bash
+#!/bin/bash -e
 # SPDX-License-Identifier: Apache-2.0
 # Script for Updating Kernel for WSL2
 # Copyright (C) Karthik Sreedevan V <sreedevan05@gmail.com>
+
+# Used by blam() to kill process from within a subprocess
+set -o pipefail
+trap "exit 1" TERM
+export TOP_PID=$$
 
 # Colours and Graphics
 blue='\033[0;34m'
@@ -29,20 +34,43 @@ printf "$yellow	  by taalojarvi@github.com\n"
 printf "$cyan$DIVIDER$nocol\n\n\n\n"
 }
 
+function blam() {
+case "$1" in
+	1) printf "$red \n *Download Failed! Please check your internet connection.$nocol\n\n"
+	   exit 1
+	   ;;
+	2) printf "$red \n *Remote kernel image not found! Aborting.$nocol\n\n" 
+	   kill -s TERM $TOP_PID
+	   ;;
+	3) printf "$red \n *Local kernel image not found! Please check your KERNEL_PATH.$nocol\n\n" 
+	   kill -s TERM $TOP_PID
+	   ;;
+	4) printf "$red \n *SHA256 failed. Aborting.$nocol\n\n"
+	   kill -s TERM $TOP_PID
+	   ;;
+	5) printf "$red \n *Copying kernel image failed. Please check your KERNEL_PATH$nocol\n\n"
+	   exit 1
+	   ;;
+	*) printf "$red \n *Achievement Unlocked! [How did we get here?] $nocol\n\n"
+	   kill -s TERM $TOP_PID
+	   ;;
+esac
+}
+
 function updater() {
 printf "$cyan *Downloading latest kernel image$nocol\n\n" 
-wget -r -q --show-progress "$KERNEL_URL" -O bzImage || printf "$red *Download failed! Check your internet Connection connection$nocol\n\n"
-printf "\n$cyan *Remote kernel image version is $(file -b bzImage | grep -o 'version [^ ]*' | cut -d ' ' -f 2 || printf "$red *Error! File not found!$nocol\n\n")$nocol\n" 
-printf "$cyan *Local kernel image version is $(file -b $KERNEL_PATH| grep -o 'version [^ ]*' | cut -d ' ' -f 2 || printf "$red *Error! File not found!$nocol\n\n")$nocol\n"
-export UPDATE_SHA=$(sha256sum bzImage | cut -d ' ' -f 1 || printf "$red *Error! Checksum failed!$nocol\n" )
-export CURRENT_SHA=$(sha256sum $KERNEL_PATH | cut -d ' ' -f 1 || printf "$red *Error! Checksum failed!$nocol\n")
+wget -r -q --show-progress "$KERNEL_URL" -O bzImage || blam 1
+printf "\n$cyan *Remote kernel image version is $(file -b bzImage | grep -o 'version [^ ]*' | cut -d ' ' -f 2 || blam 2)" || blam 2
+printf "\n$cyan *Local kernel image version is $(file -b $KERNEL_PATH| grep -o 'version [^ ]*' | cut -d ' ' -f 2 || blam 3)" || blam 3
+export UPDATE_SHA=$(sha256sum bzImage | cut -d ' ' -f 1 || blam 4 )
+export CURRENT_SHA=$(sha256sum $KERNEL_PATH | cut -d ' ' -f 1 || blam 4)
 
 if [ "$UPDATE_SHA" = "$CURRENT_SHA" ]; then
 		printf "\n$green *Kernel is up to date! No actions were taken.$nocol\n\n" 
 		rm bzImage
 	else
 		printf "\n$blue *An update was found! Installing...$nocol\n\n"
-		mv -f -v bzImage "$KERNEL_PATH" || printf "$red *Copying failed!$nocol\n\n"
+		mv -f -v bzImage "$KERNEL_PATH" || blam 5
 		printf "\n$green *Kernel was succesfully updated! Please restart your WSL2 instance.$nocol\n\n"
 fi
 }
