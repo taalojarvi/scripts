2c3c559406ad9602127a2f6bec78140e87f20037
taalojarvi <sreedevan05@gmail.com>
2022-06-27 15:52:29 +0530

    wsl_updater.sh: Add some line breaks and comments
    Signed-off-by: taalojarvi <sreedevan05@gmail.com>

--- wsl_updater.sh
+++ wsl_updater.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -e
 # SPDX-License-Identifier: Apache-2.0
 # Script for Updating Kernel for WSL2
+# Last Updated on 27th June 2022 03:49 PM IST
 # Copyright (C) Karthik Sreedevan V <sreedevan05@gmail.com>
 
-# Used by blam() to kill process from within a subprocess
+# Used by blam() to kill main process from within a subprocess
 set -o pipefail
 trap "exit 1" TERM
 export TOP_PID=$$
@@ -27,13 +28,16 @@ export KERNEL_PATH=/mnt/c/Users/sreed/bzImage
 #*************************************
 # Careful modifying beyond this point!
 #*************************************
-function banner() {
+
+# Graphic displayed on execution 
+function banner() { 
 printf "\n\n$cyan$DIVIDER$nocol\n"
 printf "$yellow	    WSL2 Kernel Updater\n"
 printf "$yellow	  by taalojarvi@github.com\n"
 printf "$cyan$DIVIDER$nocol\n\n\n\n"
 }
 
+# Command-fail state handler
 function blam() {
 case "$1" in
 	1) printf "$red \n *Download Failed! Please check your internet connection.$nocol\n\n"
@@ -57,11 +61,14 @@ case "$1" in
 esac
 }
 
+# updater function
 function updater() {
 printf "$cyan *Downloading latest kernel image$nocol\n\n" 
 wget -r -q --show-progress "$KERNEL_URL" -O bzImage || blam 1
+
 printf "\n$cyan *Remote kernel image version is $(file -b bzImage | grep -o 'version [^ ]*' | cut -d ' ' -f 2 || blam 2)" || blam 2
 printf "\n$cyan *Local kernel image version is $(file -b $KERNEL_PATH| grep -o 'version [^ ]*' | cut -d ' ' -f 2 || blam 3)" || blam 3
+
 export UPDATE_SHA=$(sha256sum bzImage | cut -d ' ' -f 1 || blam 4 )
 export CURRENT_SHA=$(sha256sum $KERNEL_PATH | cut -d ' ' -f 1 || blam 4)
 
@@ -69,10 +76,11 @@ if [ "$UPDATE_SHA" = "$CURRENT_SHA" ]; then
 		printf "\n$green *Kernel is up to date! No actions were taken.$nocol\n\n" 
 		rm bzImage
 	else
-		printf "\n$blue *An update was found! Installing...$nocol\n\n"
+		printf "\n\n$blue *An update was found! Installing...$nocol\n\n"
 		mv -f -v bzImage "$KERNEL_PATH" || blam 5
 		printf "\n$green *Kernel was succesfully updated! Please restart your WSL2 instance.$nocol\n\n"
 fi
+
 }
 
 banner
