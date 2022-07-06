25956d424b40a1e0466fd3148a7bc5e1b290d779
taalojarvi <sreedevan05@gmail.com>
2022-07-06 23:23:28 +0530

    wsl_updater.sh: Add a TUI if dialog package is present
    Adds a framework for displaying dialog boxes with the dialog package.
    Initial implementation in whiptail was dropped due to limitations and was subsequently replaced with dialog.
    Could be extended to be as verbose as the normal shell mode. Current implementation looks pretty adequate however
    
    TODO: Add Zenity and Whiptail modes
    
    Signed-off-by: taalojarvi <sreedevan05@gmail.com>

--- wsl_updater.sh
+++ wsl_updater.sh
@@ -17,13 +17,17 @@ red='\033[0;31m'
 nocol='\033[0m'
 green='\e[32m'
 DIVIDER="$blue***********************************************$nocol"
+TITLE="WSL Kernel Updater"
+BACKTITLE="Copyright(C) 2022 Karthik Sreedevan V"
+# Not yet implemented
+WHIPARGS='--title "WSL Kernel Updater" --backtitle "Copyright(C) 2022 Karthik Sreedevan V"'
 
 # Add link to the Github Releases
 # Use latest specified release format as stipulated in https://docs.github.com/en/repositories/releasing-projects-on-github/linking-to-releases
-export KERNEL_URL=https://github.com/Locietta/xanmod-kernel-WSL2/releases/latest/download/bzImage
+KERNEL_URL=https://github.com/Locietta/xanmod-kernel-WSL2/releases/latest/download/bzImage
 
 # Modify with the path to your bzImage
-export KERNEL_PATH=/mnt/c/Users/sreed/bzImage
+KERNEL_PATH=/mnt/c/Users/sreed/bzImage
 
 #*************************************
 # Careful modifying beyond this point!
@@ -37,28 +41,64 @@ printf "$yellow	  by taalojarvi@github.com\n"
 printf "$cyan$DIVIDER$nocol\n\n\n\n"
 }
 
+# dialog macros
+function whipmsg(){
+dialog --title "$TITLE" --backtitle "$BACKTITLE" --msgbox "$1" $2 $3 
+}
+
+function whipgauge() {
+dialog --title "$TITLE" --backtitle "$BACKTITLE" --gauge "$1" $2 $3 $4
+}
+
+function whipinfo() {
+dialog --title "$TITLE" --backtitle "$BACKTITLE" --infobox "$1" $2 $3 
+}
+
 # Command-fail state handler
 function blam() {
-case "$1" in
-	1) printf "$red \n *Download Failed! Please check your internet connection.$nocol\n\n"
-	   exit 1
-	   ;;
-	2) printf "$red \n *Remote kernel image not found! Aborting.$nocol\n\n" 
-	   kill -s TERM $TOP_PID
-	   ;;
-	3) printf "$red \n *Local kernel image not found! Please check your KERNEL_PATH.$nocol\n\n" 
-	   kill -s TERM $TOP_PID
-	   ;;
-	4) printf "$red \n *SHA256 failed. Aborting.$nocol\n\n"
-	   kill -s TERM $TOP_PID
-	   ;;
-	5) printf "$red \n *Copying kernel image failed. Please check your KERNEL_PATH$nocol\n\n"
-	   exit 1
-	   ;;
-	*) printf "$red \n *Achievement Unlocked! [How did we get here?] $nocol\n\n"
-	   kill -s TERM $TOP_PID
-	   ;;
-esac
+if ![[ $(command -v dialog) ]]; then
+	case "$1" in
+		1) printf "$red \n *Download Failed! Please check your internet connection.$nocol\n\n"
+	   	   exit 1
+   	   	   ;;
+		2) printf "$red \n *Remote kernel image not found! Aborting.$nocol\n\n" 
+   	   	   kill -s TERM $TOP_PID
+   	   	   ;;
+		3) printf "$red \n *Local kernel image not found! Please check your KERNEL_PATH.$nocol\n\n" 
+   	   	   kill -s TERM $TOP_PID
+   	   	   ;;
+		4) printf "$red \n *SHA256 failed. Aborting.$nocol\n\n"
+  	   	   kill -s TERM $TOP_PID
+  	   	   ;;
+		5) printf "$red \n *Copying kernel image failed. Please check your KERNEL_PATH$nocol\n\n"
+		   exit 1
+		   ;;
+		*) printf "$red \n *Achievement Unlocked! [How did we get here?] $nocol\n\n"
+		   kill -s TERM $TOP_PID
+		   ;;
+	esac
+else
+	case "$1" in
+		1) clear && whipmsg "Download Failed! Please check your internet connection." 5 75 
+	   	   exit 1
+   	   	   ;;
+		2) clear && whipmsg "Remote kernel image not found! Aborting." 5 75 
+   	   	   kill -s TERM $TOP_PID
+   	   	   ;;
+		3) clear && whipmsg "Local kernel image not found! Please check your KERNEL_PATH." 5 75 
+   	   	   kill -s TERM $TOP_PID
+   	   	   ;;
+		4) clear && whipmsg "SHA256 failed. Aborting." 5 75 
+  	   	   kill -s TERM $TOP_PID
+  	   	   ;;
+		5) clear && whipmsg "Copying kernel image failed. Please check your KERNEL_PATH" 5 75 
+		   exit 1
+		   ;;
+		*) clear && whipmsg "Achievement Unlocked! [How did we get here?]" 5 75 
+		   kill -s TERM $TOP_PID
+		   ;;
+	esac
+fi
 }
 
 # updater function
@@ -69,8 +109,8 @@ wget -r -q --show-progress "$KERNEL_URL" -O bzImage || blam 1
 printf "\n$cyan *Remote kernel image version is $(file -b bzImage | grep -o 'version [^ ]*' | cut -d ' ' -f 2 || blam 2)" || blam 2
 printf "\n$cyan *Local kernel image version is $(file -b $KERNEL_PATH| grep -o 'version [^ ]*' | cut -d ' ' -f 2 || blam 3)" || blam 3
 
-export UPDATE_SHA=$(sha256sum bzImage | cut -d ' ' -f 1 || blam 4 )
-export CURRENT_SHA=$(sha256sum $KERNEL_PATH | cut -d ' ' -f 1 || blam 4)
+UPDATE_SHA=$(sha256sum bzImage | cut -d ' ' -f 1 || blam 4 )
+CURRENT_SHA=$(sha256sum $KERNEL_PATH | cut -d ' ' -f 1 || blam 4)
 
 if [ "$UPDATE_SHA" = "$CURRENT_SHA" ]; then
 		printf "\n$green *Kernel is up to date! No actions were taken.$nocol\n\n" 
@@ -80,8 +120,28 @@ if [ "$UPDATE_SHA" = "$CURRENT_SHA" ]; then
 		mv -f -v bzImage "$KERNEL_PATH" || blam 5
 		printf "\n$green *Kernel was succesfully updated! Please restart your WSL2 instance.$nocol\n\n"
 fi
-
 }
 
-banner
-updater
+# Update with dialog boxes
+function whipdater() {
+clear && whipinfo "Checking for updates..." 3 50  
+wget -r -q "$KERNEL_URL" -O bzImage || blam 1
+UPDATE_SHA=$(sha256sum bzImage | cut -d ' ' -f 1 || blam 4 )
+CURRENT_SHA=$(sha256sum $KERNEL_PATH | cut -d ' ' -f 1 || blam 4 )
+
+	if [ "$UPDATE_SHA" = "$CURRENT_SHA" ]; then
+		clear && whipmsg "Kernel is up to date! No actions were taken." 5 50  
+		rm bzImage
+	else
+		clear && whipinfo "An update was found! Installing..." 3 50 
+		mv -f bzImage "$KERNEL_PATH" || blam 5
+		clear && whipmsg "Update completed!" 5 50 
+	fi 
+}
+
+if [[ $(command -v dialog) ]]; then
+	whipdater
+else
+	banner
+	updater
+fi
